a5b894f47efe883b4b69bb748d4506cc972fc50d
Ian Seiler <SeilerI@cwu.edu>
2018-06-08 09:59:06 -0700

    Initial comit of analysis
    importing of dataset

---
+++ Analysis.R
@@ -0,0 +1,34 @@
+##############################################################
+#Ian's Thesis Project
+##############################################################
+##############################################################
+#Install packages
+if(!require(nlme)){install.packages("nlme")}
+if(!require(multcomp)){install.packages("multcomp")}
+if(!require(multcompView)){install.packages("multcompView")}
+if(!require(lsmeans)){install.packages("lsmeans")}
+if(!require(lme4)){install.packages("lme4")}
+if(!require(lmerTest)){install.packages("lmerTest")}
+if(!require(lmerTest)){install.packages("lmerTest")}
+if(!require(TukeyC)){install.packages("TukeyC")}
+if(!require(dplyr)){install.packages("dplyr")}
+if(!require(FSA)){install.packages("FSA")}
+if(!require(car)){install.packages("car")}
+if(!require(agricolae)){install.packages("agricolae")}
+if(!require(multcomp)){install.packages("multcomp")}
+if(!require(DescTools)){install.packages("DescTools")}
+if(!require(Rmisc)){install.packages("Rmisc")}
+if(!require(ggplot2)){install.packages("ggplot2")}
+if(!require(pwr)){install.packages("pwr")}
+
+
+# ##############################################################
+#Import *.csv data
+
+rd <- read.csv("C:/Users/seileri/Desktop/R data/RD calc.csv")
+
+View(rd)
+str(rd)
+
+
+
